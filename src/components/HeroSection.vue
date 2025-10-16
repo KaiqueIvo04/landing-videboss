b971f4412dec4001a58918171df39792a83243d2
Kaique Ivo <kaiqueivo1011@gmail.com>
2025-10-15 21:04:20 -0300

    First commit

---
+++ src/components/HeroSection.vue
@@ -0,0 +1,13 @@
+<template>
+  <section id="home" class="pt-24 text-center p-10">
+    <h1 class="text-3xl font-bold text-pink-700 mb-4">
+      Nova coleção Vide Boss 👑
+    </h1>
+    <p class="text-gray-600 mb-6">Moda feminina com estilo e conforto.</p>
+    <button
+      class="btn btn-outline btn-secondary"
+    >
+      Ver coleção
+    </button>
+  </section>
+</template>
